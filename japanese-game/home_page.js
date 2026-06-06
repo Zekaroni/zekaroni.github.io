d69db897279cc41dcaf99a3513ad508d1a5baac2
Zekaroni <ezekiel.hall.04@gmail.com>
2026-06-06 19:16:21 -0400

    refactored code

--- japanese-game/home_page.js
+++ japanese-game/home_page.js
@@ -5,26 +5,28 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthState
 // TODO: Have the user sign up with an email and pick a username when registering. Make sure a check is added so there are no duplicate names
 const getPseudoEmail = (username) => `${username.trim().replace(/\s+/g, '').toLowerCase()}@hiraganamatch.internal`;
 
-onAuthStateChanged(auth, (user) =>
-{
-    if (user)
+onAuthStateChanged(auth,
+    (user) =>
     {
-        document.getElementById('user-status').textContent = "Logged in as: " + user.email.split('@')[0];
-        document.getElementById('btn-profile').classList.remove('disabled-btn');
-        document.getElementById('btn-profile').onclick = () => window.location.href = 'profile.html';
-        
-        document.getElementById('btn-login-ui').classList.add('hidden');
-        document.getElementById('btn-logout-ui').classList.remove('hidden');
-        document.getElementById('auth-modal').classList.add('hidden');
-    } else {
-        document.getElementById('user-status').textContent = "Not logged in";
-        document.getElementById('btn-profile').classList.add('disabled-btn');
-        document.getElementById('btn-profile').onclick = null;
-        
-        document.getElementById('btn-login-ui').classList.remove('hidden');
-        document.getElementById('btn-logout-ui').classList.add('hidden');
+        if (user)
+        {
+            document.getElementById('user-status').textContent = "Logged in as: " + user.email.split('@')[0];
+            document.getElementById('btn-profile').classList.remove('disabled-btn');
+            document.getElementById('btn-profile').onclick = () => window.location.href = 'profile.html';
+            
+            document.getElementById('btn-login-ui').classList.add('hidden');
+            document.getElementById('btn-logout-ui').classList.remove('hidden');
+            document.getElementById('auth-modal').classList.add('hidden');
+        } else {
+            document.getElementById('user-status').textContent = "Not logged in";
+            document.getElementById('btn-profile').classList.add('disabled-btn');
+            document.getElementById('btn-profile').onclick = null;
+            
+            document.getElementById('btn-login-ui').classList.remove('hidden');
+            document.getElementById('btn-logout-ui').classList.add('hidden');
+        }
     }
-});
+);
 
 // UI Listeners
 document.getElementById('btn-login-ui').onclick = () =>
